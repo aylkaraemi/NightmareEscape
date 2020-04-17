a06f4c49951a22ae7d51e8e3c77ea846de2e7934
Kayla Carpenter <aylkaraemi@gmail.com>
2020-04-17 10:49:08 -0600

    Add basic player movement functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed;
     private float horizInput;
     private float vertInput;
     // Start is called before the first frame update
@@ -16,6 +16,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizInput = Input.GetAxis("Horizontal");
+        vertInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * vertInput * Time.deltaTime * speed);
     }
 }
