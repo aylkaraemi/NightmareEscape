92a628c95961d273fc8878614a226b623f22436a
Kayla Carpenter <aylkaraemi@gmail.com>
2020-05-07 18:37:04 -0600

    Tweak - Adjust camera position values

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,11 +6,10 @@ public class CameraMovement : MonoBehaviour
 {
     private GameObject player;
 
-    private float height = 2f;
+    private float height = 5f;
     private float distance;
     private Vector3 prevPos, moveDir;
-    private float xRotation = 15f;
-    private Vector3 offset = new Vector3(0.0f, 4.0f, -6.0f);
+    private Vector3 offset = new Vector3(0.0f, 6.0f, -6.0f);
 
     // Start is called before the first frame update
     void Start()
@@ -38,7 +37,7 @@ public class CameraMovement : MonoBehaviour
             // have not yet figured out a solution to smooth this that doesn't make the rotation
             // when moving odd (curving a little too far on turns, like the camera behind a car in
             // a driving game, but really weird for following a person)
-            
+
             prevPos = player.transform.position;
         }        
     }
